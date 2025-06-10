1f4f09b6bc86194f6452225fe7561ca1024da701
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-06-09 21:44:47 -0400

    input add item functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,34 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  //create an array, and for each item in that array we'll create (and display) a <li> element
-  const todoList = ['this', 'is', 'a', 'test'];
+  const [todo, setToDo] = useState(''); //initializes value of the input to empty string
+  const [todos, setToDos] = useState([]);
 
-  const listElements = todoList.map((item) => {
-    return <li>{item}</li>;
+  const listElements = todos.map((item, index) => {
+    <li key={index}>{item}</li>;
   });
+
+  function handleChange(e) {
+    setToDo(e.target.value);
+  }
+
+  function handleAddToDo() {
+    if (todo.trim() === '') return; //prevents adding empty tasks
+    setToDos((prevToDo) => {
+      [...prevToDo, todo];
+    });
+    setToDo(''); //clears input
+  }
 
   return (
     <>
       <div className='form--container'>
         <h1>TODO List</h1>
         <form>
-          <input type='text' placeholder='Add something to your list'></input>
-          <button>Add to List</button>
+          <input type='text' placeholder='Add something to your list' value={todo} onChange={handleChange}></input>
+          <button onClick={handleAddToDo}>Add to List</button>
         </form>
+        <div className='list--container'>{listElements}</div>
       </div>
     </>
   );
